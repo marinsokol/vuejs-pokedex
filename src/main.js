6064ac3dcc68673d74e9fca8b2caf9e079fa59a4
Marin Markovic <marin.sokol@gmail.com>
2018-05-18 19:17:56 +0200

    init router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import App from './App.vue'
+import Layout from './components/Layout.vue'
 
 Vue.config.productionTip = false
 
+const routes = [
+  { path: '/', component: Layout },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes // short for `routes: routes`
+})
+
+Vue.use(VueRouter)
 new Vue({
+   el: '#app',
+  router,
+  // replace the content of <div id="app"></div> with App
   render: h => h(App)
 }).$mount('#app')
